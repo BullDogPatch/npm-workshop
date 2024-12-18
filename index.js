df2e5adcabe4fd23c90be7d61dc925023796974c
Craig Clayton <craig__clayton@outlook.com>
2024-12-18 13:12:48 +0000

    play around with some of the colours

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
+
+console.log(
+  chalk.underline.bgGreen('Craig') + ' ' + chalk.underline.bgRed('Clayton')
+);
+
+const firstName = 'Craig';
+const isTheBestDeveloper = false;
+const error = chalk.blue.bold.bgRed;
+
+if (isTheBestDeveloper) {
+  console.log(`${chalk.blue.bold.bgGreen(firstName)} is the best developer`);
+} else {
+  console.log(`${error(firstName)} is NOT the best developer`);
+}
